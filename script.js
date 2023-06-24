0d4c2c74c4179d8da70bfa0bd10d81e26fd623f2
Shailee-11 <128290114+Shailee-11@users.noreply.github.com>
2023-06-24 13:54:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+ const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+const soundsDir = 'sounds/';
+
+let currentSound = null; // keep track of the currently playing sound
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // stop the current sound, if there is one
+	  console.log("play");
+  })};
+ stopButton.addEventListener('click', () => {
+    // stop the current sound, if there is one
+	  console.log("stop");
+  })
